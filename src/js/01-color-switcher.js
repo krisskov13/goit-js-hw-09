d8dc6571a06ca176b56d58e6fed80625a0159140
krisskov13 <kristinakovalenko333@gmail.com>
2023-11-15 17:23:36 +0200

    update

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
 
+const startButton = document.querySelector('[data-start]');
+const stopButton = document.querySelector('[data-stop]');
+
+let colorChangeInterval;
+
+function changeBackgroundColor() {
+    document.body.style.backgroundColor = getRandomHexColor();
+}
+
+function onStartButtonClick() {
+    startButton.disabled = true;
+    stopButton.disabled = false;
+
+    colorChangeInterval = setInterval(changeBackgroundColor, 1000);
+}
+
+function onStopButtonClick() {
+    stopButton.disabled = true;
+    startButton.disabled = false;
+
+    clearInterval(colorChangeInterval);
+}
+
+startButton.addEventListener('click', onStartButtonClick);
+stopButton.addEventListener('click', onStopButtonClick);
