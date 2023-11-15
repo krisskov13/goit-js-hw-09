b9e220623a70d5c48fd8277bf530e18c41837820
krisskov13 <126875457+krisskov13@users.noreply.github.com>
2023-11-15 15:23:59 +0000

    Deploying to gh-pages from @ krisskov13/goit-js-hw-09@d8dc6571a06ca176b56d58e6fed80625a0159140 🚀

---
+++ 01-color-switcher.6ea14e85.js
@@ -0,0 +1,2 @@
+!function(){var t,e=document.querySelector("[data-start]"),a=document.querySelector("[data-stop]");function d(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}e.addEventListener("click",(function(){e.disabled=!0,a.disabled=!1,t=setInterval(d,1e3)})),a.addEventListener("click",(function(){a.disabled=!0,e.disabled=!1,clearInterval(t)}))}();
+//# sourceMappingURL=01-color-switcher.6ea14e85.js.map
